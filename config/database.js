34267e1c975f6573eb05e9bd2cb1ce93503e5f93
nguyenbach2810 <nguyenbach2810@gmail.com>
2016-04-02 18:20:00 +0700

    change from local to mongolab tadabase

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/' 
-
+    'url' : 'mongodb://adminnguyenbach:Garfield123@ds011840.mlab.com:11840/testdatabase' 
+    //mongodb://localhost/     
 };
